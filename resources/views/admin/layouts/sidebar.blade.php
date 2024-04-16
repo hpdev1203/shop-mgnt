9e233b8448652e4109523c000fcfc3701b6332f6
hpdev1203 <67946108+hpdev1203@users.noreply.github.com>
2024-04-16 17:09:10 +0700

    add data for user logged in

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -5,8 +5,8 @@
     >
     <!-- SIDEBAR HEADER -->
     <div class="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-        <a href="index.html">
-        <img src="{{asset('library/images/logo/logo.svg')}}" alt="Logo" />
+        <a href="{{route('admin')}}">
+            <img src="{{asset('library/images/logo/logo.svg')}}" alt="Logo" />
         </a>
         <button
             class="block lg:hidden"
@@ -33,7 +33,7 @@
         >
         <!-- Sidebar Menu -->
         <nav
-            class="mt-5 px-4 py-4 lg:mt-9 lg:px-6"
+            class="px-4 py-4 lg:px-6"
             x-data="{selected: $persist('Dashboard')}"
             >
             <div>
@@ -41,7 +41,7 @@
                     <!-- Menu Item Dashboard -->
                     <li>
                         <a
-                            class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                            class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 text-white"
                             href="{{route('admin')}}"
                             >
                             <svg
@@ -73,39 +73,14 @@
                         </a>
                     </li>
                     <!-- Menu Item Dashboard -->
-                    <!-- Menu Item Calendar -->
-                    <li>
-                        <a
-                            class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
-                            href="calendar.html"
-                            @click="selected = (selected === 'Calendar' ? '':'Calendar')"
-                            :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Calendar') && (page === 'calendar') }"
-                            >
-                            <svg
-                                class="fill-current"
-                                width="18"
-                                height="18"
-                                viewBox="0 0 18 18"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                >
-                                <path
-                                    d="M15.7499 2.9812H14.2874V2.36245C14.2874 2.02495 14.0062 1.71558 13.6405 1.71558C13.2749 1.71558 12.9937 1.99683 12.9937 2.36245V2.9812H4.97803V2.36245C4.97803 2.02495 4.69678 1.71558 4.33115 1.71558C3.96553 1.71558 3.68428 1.99683 3.68428 2.36245V2.9812H2.2499C1.29365 2.9812 0.478027 3.7687 0.478027 4.75308V14.5406C0.478027 15.4968 1.26553 16.3125 2.2499 16.3125H15.7499C16.7062 16.3125 17.5218 15.525 17.5218 14.5406V4.72495C17.5218 3.7687 16.7062 2.9812 15.7499 2.9812ZM1.77178 8.21245H4.1624V10.9968H1.77178V8.21245ZM5.42803 8.21245H8.38115V10.9968H5.42803V8.21245ZM8.38115 12.2625V15.0187H5.42803V12.2625H8.38115ZM9.64678 12.2625H12.5999V15.0187H9.64678V12.2625ZM9.64678 10.9968V8.21245H12.5999V10.9968H9.64678ZM13.8374 8.21245H16.228V10.9968H13.8374V8.21245ZM2.2499 4.24683H3.7124V4.83745C3.7124 5.17495 3.99365 5.48433 4.35928 5.48433C4.7249 5.48433 5.00615 5.20308 5.00615 4.83745V4.24683H13.0499V4.83745C13.0499 5.17495 13.3312 5.48433 13.6968 5.48433C14.0624 5.48433 14.3437 5.20308 14.3437 4.83745V4.24683H15.7499C16.0312 4.24683 16.2562 4.47183 16.2562 4.75308V6.94683H1.77178V4.75308C1.77178 4.47183 1.96865 4.24683 2.2499 4.24683ZM1.77178 14.5125V12.2343H4.1624V14.9906H2.2499C1.96865 15.0187 1.77178 14.7937 1.77178 14.5125ZM15.7499 15.0187H13.8374V12.2625H16.228V14.5406C16.2562 14.7937 16.0312 15.0187 15.7499 15.0187Z"
-                                    fill=""
-                                    />
-                            </svg>
-                            Calendar
-                        </a>
-                    </li>
-                    <!-- Menu Item Calendar -->
+
                     <!-- Menu Item Profile -->
                     <li>
                         <a
                             class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
-                            href="profile.html"
-                            @click="selected = (selected === 'Profile' ? '':'Profile')"
-                            :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Profile') && (page === 'profile') }"
-                            :class="page === 'profile' && 'bg-graydark'"
+                            href="#"
+                            @click.prevent="selected = (selected === 'Users' ? '':'Users')"
+                            :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Users') || (page === 'formElements' || page === 'formLayout') }"
                             >
                             <svg
                                 class="fill-current"
@@ -124,48 +99,83 @@
                                     fill=""
                                     />
                             </svg>
-                            Profile
+                            Người dùng
+                            <svg
+                                class="absolute right-4 top-1/2 -translate-y-1/2 fill-current"
+                                :class="{ 'rotate-180': (selected === 'Users') }"
+                                width="20"
+                                height="20"
+                                viewBox="0 0 20 20"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                >
+                                <path
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                    d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                                    fill=""
+                                    />
+                            </svg>
                         </a>
+                        <div
+                            class="translate transform overflow-hidden"
+                            :class="(selected === 'Users') ? 'block' :'hidden'"
+                            >
+                            <ul class="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
+                                <li>
+                                    <a
+                                        class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                                        href="form-elements.html"
+                                        :class="page === 'formElements' && '!text-white'"
+                                        >Tạo</a
+                                        >
+                                </li>
+                                <li>
+                                    <a
+                                        class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                                        href="form-layout.html"
+                                        :class="page === 'formLayout' && '!text-white'"
+                                        >Danh sách</a
+                                        >
+                                </li>
+                            </ul>
+                        </div>
                     </li>
                     <!-- Menu Item Profile -->
-                    <!-- Menu Item Forms -->
+                    <!-- Menu Item Product -->
                     <li>
                         <a
                             class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
                             href="#"
                             @click.prevent="selected = (selected === 'Forms' ? '':'Forms')"
                             :class="{ 'bg-graydark dark:bg-meta-4': (selected === 'Forms') || (page === 'formElements' || page === 'formLayout') }"
-                            >
+                        >
                             <svg
                                 class="fill-current"
                                 width="18"
-                                height="18"
-                                viewBox="0 0 18 18"
+                                height="19"
+                                viewBox="0 0 18 19"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
                                 >
-                                <path
-                                    d="M1.43425 7.5093H2.278C2.44675 7.5093 2.55925 7.3968 2.58737 7.31243L2.98112 6.32805H5.90612L6.27175 7.31243C6.328 7.48118 6.46862 7.5093 6.58112 7.5093H7.453C7.76237 7.48118 7.87487 7.25618 7.76237 7.03118L5.428 1.4343C5.37175 1.26555 5.3155 1.23743 5.14675 1.23743H3.88112C3.76862 1.23743 3.59987 1.29368 3.57175 1.4343L1.153 7.08743C1.0405 7.2843 1.20925 7.5093 1.43425 7.5093ZM4.47175 2.98118L5.3155 5.17493H3.59987L4.47175 2.98118Z"
-                                    fill=""
-                                    />
-                                <path
-                                    d="M10.1249 2.5031H16.8749C17.2124 2.5031 17.5218 2.22185 17.5218 1.85623C17.5218 1.4906 17.2405 1.20935 16.8749 1.20935H10.1249C9.7874 1.20935 9.47803 1.4906 9.47803 1.85623C9.47803 2.22185 9.75928 2.5031 10.1249 2.5031Z"
-                                    fill=""
-                                    />
-                                <path
-                                    d="M16.8749 6.21558H10.1249C9.7874 6.21558 9.47803 6.49683 9.47803 6.86245C9.47803 7.22808 9.75928 7.50933 10.1249 7.50933H16.8749C17.2124 7.50933 17.5218 7.22808 17.5218 6.86245C17.5218 6.49683 17.2124 6.21558 16.8749 6.21558Z"
-                                    fill=""
-                                    />
-                                <path
-                                    d="M16.875 11.1656H1.77187C1.43438 11.1656 1.125 11.4469 1.125 11.8125C1.125 12.1781 1.40625 12.4594 1.77187 12.4594H16.875C17.2125 12.4594 17.5219 12.1781 17.5219 11.8125C17.5219 11.4469 17.2125 11.1656 16.875 11.1656Z"
-                                    fill=""
-                                    />
-                                <path
-                                    d="M16.875 16.1156H1.77187C1.43438 16.1156 1.125 16.3969 1.125 16.7625C1.125 17.1281 1.40625 17.4094 1.77187 17.4094H16.875C17.2125 17.4094 17.5219 17.1281 17.5219 16.7625C17.5219 16.3969 17.2125 16.1156 16.875 16.1156Z"
-                                    fill="white"
-                                    />
+                                <g clip-path="url(#clip0_130_9756)">
+                                    <path
+                                        d="M15.7501 0.55835H2.2501C1.29385 0.55835 0.506348 1.34585 0.506348 2.3021V15.8021C0.506348 16.7584 1.29385 17.574 2.27822 17.574H15.7782C16.7345 17.574 17.5501 16.7865 17.5501 15.8021V2.3021C17.522 1.34585 16.7063 0.55835 15.7501 0.55835ZM6.69385 10.599V6.4646H11.3063V10.5709H6.69385V10.599ZM11.3063 11.8646V16.3083H6.69385V11.8646H11.3063ZM1.77197 6.4646H5.45635V10.5709H1.77197V6.4646ZM12.572 6.4646H16.2563V10.5709H12.572V6.4646ZM2.2501 1.82397H15.7501C16.0313 1.82397 16.2563 2.04897 16.2563 2.33022V5.2271H1.77197V2.3021C1.77197 2.02085 1.96885 1.82397 2.2501 1.82397ZM1.77197 15.8021V11.8646H5.45635V16.3083H2.2501C1.96885 16.3083 1.77197 16.0834 1.77197 15.8021ZM15.7501 16.3083H12.572V11.8646H16.2563V15.8021C16.2563 16.0834 16.0313 16.3083 15.7501 16.3083Z"
+                                        fill=""
+                                        />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_130_9756">
+                                        <rect
+                                            width="18"
+                                            height="18"
+                                            fill="white"
+                                            transform="translate(0 0.052124)"
+                                            />
+                                    </clipPath>
+                                </defs>
                             </svg>
-                            Forms
+                            Quản lý sản phẩm
                             <svg
                                 class="absolute right-4 top-1/2 -translate-y-1/2 fill-current"
                                 :class="{ 'rotate-180': (selected === 'Forms') }"
