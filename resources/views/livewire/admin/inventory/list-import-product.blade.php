65c893f2e8c83728f9113065dda918f8fb151a83
nguyenxuanmin <nguyenxuanmin1998@gmail.com>
2024-04-23 21:29:17 +0700

    update import product

--- resources/views/livewire/admin/inventory/list-import-product.blade.php
+++ resources/views/livewire/admin/inventory/list-import-product.blade.php
@@ -31,7 +31,7 @@
                     <td class="px-6 py-2 whitespace-nowrap">{{$import_product->name}}</td>
                     <td class="px-6 py-2 whitespace-nowrap">{{$import_product->warehouse_id}}</td>
                     <td class="px-6 py-2 whitespace-nowrap text-center">
-                        <a href="{{route('admin.users.edit', $import_product->id)}}" class="inline-flex items-center mr-2 text-indigo-600 hover:text-indigo-900">
+                        <a href="{{route('admin.import-product.edit', $import_product->id)}}" class="inline-flex items-center mr-2 text-indigo-600 hover:text-indigo-900">
                             <svg class="icon" data-bs-toggle="tooltip" data-bs-title="Edit" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                 <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
