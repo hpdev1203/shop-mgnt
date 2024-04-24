053baa639054241353e0467ac473e50331665960
hpdev1203 <67946108+hpdev1203@users.noreply.github.com>
2024-04-24 18:16:27 +0700

    Updated total order and stock

--- app/Livewire/Admin/Category/ListCategory.php
+++ app/Livewire/Admin/Category/ListCategory.php
@@ -12,17 +12,35 @@ class ListCategory extends Component
     use WithPagination, WithoutUrlPagination; 
 
     public $search_input = '';
+    public $list_category = [];
+    public $selected_index = [];
 
     public function search()
     {
         $this->resetPage();
     }
 
-    public function handleDetele($id)
+    public function deleteListCheckbox()
     {
+        foreach ($this->selected_index as $key => $checked) {
+            if($checked == true){
+                $category_id = $this->list_category[$key]['id'];
+                $this->deleteCategory($category_id);
+            }
+        }
+        $this->selected_index = [];
+        $this->render();
+    }
+
+    public function deleteCategory($id){
         $category = Category::find($id);
         $category->delete();
         session()->flash('success', 'Category deleted successfully');
+    }
+
+    public function handleDetele($id)
+    {
+        $this->deleteCategory($id);
         $this->render();
     }
 
@@ -33,6 +51,8 @@ class ListCategory extends Component
         }else{
             $categories = Category::where('name', 'like', '%'.$this->search_input.'%')->orWhere('description', 'like', '%'.$this->search_input.'%')->paginate(10);
         }
+
+        $this->list_category = collect($categories->items());
         return view('livewire.admin.category.list-category', ['categories' => $categories]);
     }
 }
