b23364c4e4177b8824cdd163561d7bb3b3046470
nguyenxuanmin <nguyenxuanmin1998@gmail.com>
2024-05-22 16:17:57 +0700

    update search

---
+++ app/Livewire/Client/Search.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire\Client;
+
+use Livewire\Component;
+use App\Models\Product;
+
+class Search extends Component
+{
+    public $input_search = '';
+
+    public function search()
+    {
+        $this->render();
+    }
+
+    public function closeSearch()
+    {
+        $this->input_search = "";
+        $this->render();
+    }
+    
+    public function render()
+    {
+        $product_search = "";
+        if($this->input_search != ''){
+            $product_search =  Product::where('name', 'like', '%'.$this->input_search.'%')
+                ->orWhere('code', 'like', '%'.$this->input_search.'%')
+                ->orderBy('name','asc')->get();
+        }
+        return view('livewire.client.search', ["product_search" => $product_search]);
+    }
+}
