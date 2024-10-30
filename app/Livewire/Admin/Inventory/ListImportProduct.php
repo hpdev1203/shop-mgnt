5b956277a78a87c2e4908eaed89aeabf6c779635
lopper72 <vanphuong.fit@gmail.com>
2024-10-30 15:26:24 +0700

    add delete import_product for m8

--- app/Livewire/Admin/Inventory/ListImportProduct.php
+++ app/Livewire/Admin/Inventory/ListImportProduct.php
@@ -13,6 +13,8 @@ class ListImportProduct extends Component
     use WithPagination, WithoutUrlPagination;
     public $search_input = '';
     public $code = '';
+    public $list_product = [];
+    public $selected_index = [];
 
     public function search()
     {
@@ -28,9 +30,30 @@ class ListImportProduct extends Component
             ->orWhere('code', 'like', '%'.$this->search_input.'%')
             ->orderBy('created_at','desc')->paginate(10);
         }
+        $this->list_product = collect($import_products->items());
         return view('livewire.admin.inventory.list-import-product', ['import_products' => $import_products]);
     }
     
+    public function deleteListCheckbox()
+    {
+        foreach ($this->selected_index as $key => $checked) {
+            if($checked == true){
+                $product_id = $this->list_product[$key]['id'];
+                $this->deleteProduct($product_id);
+            }
+        }
+        $this->selected_index = [];
+        $this->render();
+    }
+    public function deleteProduct($id){
+        $product = ImportProduct::find($id);
+        $product_detail = ImportProductDetail::where('import_product_id', $id)->get();
+        foreach ($product_detail as $item) {
+            $item->delete();
+        }
+        $product->delete();
+    }
+
     public function updateImportProduct($id,$code){
         ImportProductDetail::where('import_product_id', $id)
             ->update([
